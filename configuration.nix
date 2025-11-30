1463a75b8f5c3b00cdbc1f9a4c131c28de887110
VOKTEX <74386536+VoktexYT@users.noreply.github.com>
2025-11-30 14:19:17 -0500

    Refactor configuration.nix for improved readability

--- configuration.nix
+++ configuration.nix
@@ -1,31 +1,31 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
+  imports = [
+    ./hardware-configuration.nix
     (builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz")
-  ];
+  ];
   
   boot.kernelPackages = pkgs.linuxPackages_zen;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  
-  networking.wireless.iwd.enable = true;
-  networking.networkmanager.enable = false;
-  virtualisation.docker.enable = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   
-  services.xserver.enable = true; 
-  services.xserver.displayManager.lightdm.enable = false;
-  
-  users.users.ton_user = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "docker" ];
-    shell = pkgs.fish; 
-  };
-  
-  environment.systemPackages = with pkgs; [
-    wl-clipboard
-  ];
+  networking.wireless.iwd.enable = true;
+  networking.networkmanager.enable = false;
+  virtualisation.docker.enable = true;
+  
+  services.xserver.enable = true;
+  services.xserver.displayManager.lightdm.enable = false;
+  
+  users.users.ton_user = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "docker" ];
+    shell = pkgs.fish; 
+  };
+  
+  environment.systemPackages = with pkgs; [
+    wl-clipboard
+  ];
 
-  home-manager.users.ton_user = import ./home.nix;
-  system.stateVersion = "24.05";
+  home-manager.users.ton_user = import ./home.nix;
+  system.stateVersion = "24.05";
 }
